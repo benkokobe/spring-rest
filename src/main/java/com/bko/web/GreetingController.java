5b664ae5734552cbcb82480536c1bd6762c208c7
Ben <ben.kokobe@gmail.com>
2015-05-07 23:24:18 +0200

    Modified GreetingControl to use @RestControl

--- src/main/java/com/bko/web/GreetingController.java
+++ src/main/java/com/bko/web/GreetingController.java
@@ -6,17 +6,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.bko.domain.Greeting;
 
-@Controller
+//@Controller
+@RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    @ResponseBody
+    //@ResponseBody
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),String.format(template, name));
     }
